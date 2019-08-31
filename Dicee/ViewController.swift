07669a0afde7f01e1b88bfc55359ba0a84bdfb6b
mohamed karmout <mohamed_karmout@icloud.com>
2019-08-31 03:34:40 +0200

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by mohamed harmout on 8/31/19.
+//  Copyright © 2019 mohamed karmout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
